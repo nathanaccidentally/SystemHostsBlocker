51f2a3773d8977e53dbfc02513f8cbc527ad8f9d
Nathan Goodwin <nathanaccidentally@gmail.com>
2016-11-10 07:57:45 -0600

    added scripts for mac and windows

---
+++ updatehosts.bat
@@ -0,0 +1,4 @@
+cd C:\Users\(Username)\Dekstop
+git clone https://github.com/nathanaccidentally/SystemHostBlocker
+cd C:\Users\(Username)\Desktop\SystemHostBlocker
+move hosts C:\Windows\System32\drivers\etc\
