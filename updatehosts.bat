0ab87602719ec8be25a34e86de9849e04198d6bc
Nathan Goodwin <nathanaccidentally@gmail.com>
2017-02-06 19:37:14 -0600

    Delete updatehosts.bat

--- updatehosts.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\(Username)\Dekstop
-git clone https://github.com/nathanaccidentally/SystemHostBlocker
-cd C:\Users\(Username)\Desktop\SystemHostBlocker
-move hosts C:\Windows\System32\drivers\etc\
